4548495864d983889b8409243d8124c73f4f75e1
JavidLalayev <cavidleleyev9a12014@gmail.com>
2020-02-01 20:05:36 +0400

    first commit

---
+++ application/views/admin/includes/left_navbar.php
@@ -0,0 +1,34 @@
+<div id="main-page">
+
+
+    <div id="left-nav-open">
+        <div id="nav-area">
+            <div class="menu-1">
+                <a href="<?php echo base_url("Standart_crud_demo")?>">
+                    <i class="fas fa-ellipsis-h"></i>
+                    <span class="m-1-span" style="display: none;">Bloq</span>
+                </a>
+            </div>
+
+
+            <div class="menu-1 drop-click">
+                <div class="drop-text">
+                    <i class="fas fa-user-circle"></i>
+                    <span class="m-1-span" style="display: none;">
+                            Hesabım
+                            <i class="fas fa-chevron-down rotated down-arrow"></i>
+                        </span>
+                </div>
+
+                <div class="menu-2">
+                    <a href="<?php echo base_url("One_row_update_demo")?>">
+                        <i class="fas fa-user-cog"></i>
+                        <span class="m-1-span" style="display: none">Haqqımızda</span>
+                    </a>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
