238054cb14db0905fb6e5a41ce425b32a844f6d5
JavidLalayev <cavidleleyev9a12014@gmail.com>
2020-02-01 20:13:26 +0400

    first commit

--- core_v2_1.sql
+++ core_v2_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 01 Oca 2020, 09:46:36
+-- Üretim Zamanı: 01 Şub 2020, 17:13:08
 -- Sunucu sürümü: 10.4.8-MariaDB
 -- PHP Sürümü: 7.3.11
 
@@ -66,7 +66,7 @@ CREATE TABLE `blog` (
 
 INSERT INTO `blog` (`id`, `blog_category_id`, `name_az`, `name_en`, `name_ru`, `desc_az`, `desc_en`, `desc_ru`, `img`) VALUES
 (3, 3, 'ad', 'asdas', 'asasd', '<p><img alt=\"\" src=\"/LuckyTravel/public/admin/ckeditor/kcfinder/upload/images/kali-theme.jpg\" style=\"height:214px; width:380px\" />aasdasass</p>\r\n', '', '', 'macbook_pro_apple_laptop_headphones_table_98893_2560x1440.jpg'),
-(10, 1, 'asdasd', 'asdas', 'adas', '', '', '<p>asda</p>\r\n', 'germany_cochem_mosel_river_castle_104431_1920x1080.jpg');
+(10, 1, 'asdasd', 'asdas', 'adas', '', '', '<p>asda</p>\r\n', 'minecraft_planet_cube_cubes_world_2228_1920x1080.jpg');
 
 -- --------------------------------------------------------
 
@@ -115,10 +115,8 @@ INSERT INTO `blog_gallery` (`id`, `blog_id`, `file`) VALUES
 (67, 1, 'world_of_tanks_t_62a_field_105518_1920x1080.jpg'),
 (69, 10, 'project_cars_lykan_hypersport_hypersport_104438_1920x1080.jpg'),
 (73, 3, 'skyrim_buildings_mountains_night_98682_1920x1080.jpg'),
-(74, 10, 'ford_mustang_ford_car_147558_1920x10801.jpg'),
-(75, 10, 'project_cars_lykan_hypersport_hypersport_104438_1920x10801.jpg'),
-(76, 10, 'race_motorcycle_sports_76595_1920x1080.jpg'),
-(86, 12, 'fallout_4_bethesda_softworks_armor_104221_1920x1080.jpg');
+(86, 12, 'fallout_4_bethesda_softworks_armor_104221_1920x1080.jpg'),
+(99, 10, '5d5bda405449c_(1).jpg');
 
 -- --------------------------------------------------------
 
@@ -137,11 +135,11 @@ CREATE TABLE `blog_includes` (
 --
 
 INSERT INTO `blog_includes` (`id`, `blog_id`, `includes_id`) VALUES
-(38, 10, 4),
-(41, 3, 4),
-(44, 12, 4),
-(45, 10, 2),
-(46, 10, 1);
+(46, 10, 1),
+(48, 3, 1),
+(52, 10, 2),
+(54, 10, 3),
+(55, 10, 4);
 
 -- --------------------------------------------------------
 
@@ -169,6 +167,56 @@ INSERT INTO `includes` (`id`, `name`, `yas`, `date`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `new_subblog`
+--
+
+CREATE TABLE `new_subblog` (
+  `id` int(11) NOT NULL,
+  `blog_id` int(10) NOT NULL,
+  `name_az` varchar(255) NOT NULL,
+  `name_en` varchar(255) NOT NULL,
+  `name_ru` varchar(255) NOT NULL,
+  `desc_az` text NOT NULL,
+  `desc_en` text NOT NULL,
+  `desc_ru` text NOT NULL,
+  `img1` varchar(254) NOT NULL,
+  `img2` varchar(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `new_subblog`
+--
+
+INSERT INTO `new_subblog` (`id`, `blog_id`, `name_az`, `name_en`, `name_ru`, `desc_az`, `desc_en`, `desc_ru`, `img1`, `img2`) VALUES
+(4, 10, 'adsd', 'asdas', 'asdas', '<p>asdas</p>\r\n', '<p>asda</p>\r\n', '<p>aas</p>\r\n', 'iphone_ipad_apple_watches_stylus_pen_style_99204_1920x1080.jpg', 'clouds_milky_way_eclipse_light_68883_2560x1440.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `sub_blog`
+--
+
+CREATE TABLE `sub_blog` (
+  `id` int(11) NOT NULL,
+  `blog_id` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `img` varchar(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `sub_blog`
+--
+
+INSERT INTO `sub_blog` (`id`, `blog_id`, `name`, `surname`, `img`) VALUES
+(13, 10, 'aasd', '', 'default.png'),
+(14, 10, '123', '', 'default.png'),
+(15, 3, 'ffff', '', 'default.png'),
+(16, 3, 'aads', 'asdasd', 'ford_mustang_ford_car_147558_1920x1080.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `users`
 --
 
@@ -185,7 +233,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `isAdmin`) VALUES
 (1, 'super_admin', '21232f297a57a5a743894a0e4a801fc3', 1),
-(3, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0);
+(3, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0),
+(4, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0),
+(5, 'admin2', 'c84258e9c39059a89ab77d846ddab909', 0),
+(6, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0),
+(7, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0);
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -229,6 +281,19 @@ ALTER TABLE `includes`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Tablo için indeksler `new_subblog`
+--
+ALTER TABLE `new_subblog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `sub_blog`
+--
+ALTER TABLE `sub_blog`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `blog_id` (`blog_id`);
+
+--
 -- Tablo için indeksler `users`
 --
 ALTER TABLE `users`
@@ -248,7 +313,7 @@ ALTER TABLE `about`
 -- Tablo için AUTO_INCREMENT değeri `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `blog_category`
@@ -260,13 +325,13 @@ ALTER TABLE `blog_category`
 -- Tablo için AUTO_INCREMENT değeri `blog_gallery`
 --
 ALTER TABLE `blog_gallery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `blog_includes`
 --
 ALTER TABLE `blog_includes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `includes`
@@ -275,10 +340,22 @@ ALTER TABLE `includes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- Tablo için AUTO_INCREMENT değeri `new_subblog`
+--
+ALTER TABLE `new_subblog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `sub_blog`
+--
+ALTER TABLE `sub_blog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
 -- Tablo için AUTO_INCREMENT değeri `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
